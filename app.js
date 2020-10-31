24a6a9a83bfda0b764aca3b6d011c0d2f2185db9
HassanKhan123 <hk2602809@gmail.com>
2020-10-31 08:37:48 +0500

    binding attriutes with v-bind directive

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const app = new Vue({
     el: '#user-goal',
     data(){
         return {
-            courseGoal:'Finish the course and learn vue'
+            courseGoal:'Finish the course and learn vue',
+            vueLink:'https://www.google.com'
         }
     }
    
