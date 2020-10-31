b4e96861cf43da998b3e4a6588f97c3ccbed224b
HassanKhan123 <hk2602809@gmail.com>
2020-10-31 08:44:26 +0500

    methods in vue apps

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const app = new Vue({
             courseGoal:'Finish the course and learn vue',
             vueLink:'https://www.google.com'
         }
+    },
+    methods:{
+        outputGoal(){
+            const randomNumber = Math.random()
+            if(randomNumber < 0.5){
+                return 'Learn Vue'
+            }else{
+                return 'Master Vue'
+            }
+        }
     }
    
   })
